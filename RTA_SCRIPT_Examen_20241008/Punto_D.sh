8496c6fcda316451f668894433b095b36193f4ad
pinkei <keilasol930@gmail.com>
2024-10-12 02:59:14 +0000

    Punto D

--- RTA_SCRIPT_Examen_20241008/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_D.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir /Estructura_Asimetrica
-mkdir /Estructura_Asimetrica/correo /Estructura_Asimetrica/clientes
-for i in {1..100}; do mkdir /Estructura_Asimetrica/correo/cartas_$i /Estructura_Asimetrica/clientes/cartas_$i; done
-for i in {1..10}; do mkdir /Estructura_Asimetrica/correo/carteros_$i; done
+cd ..
+cd ..
+pwd
+home/vagrant
+sudo mkdir -p /Estructura_Asimetrica/{{correo/cartas{1..100},carteros{1..10}},clientes{cartas{1..100}}}
